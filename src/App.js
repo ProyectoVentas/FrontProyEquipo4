b537f665e4e87866b82a2fccdc683b9637d75064
Andres_Coronado <andrescoronadob5@gmail.com>
2021-10-25 23:01:48 +1100

    front proy coregido

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//private
+import PrivateRoute from "./components/routing/PrivateRoute";
+import HomeRoute from "./components/routing/HomeRoute";
+import ProductsRoute from "./components/routing/ProductsRoute";
+import UserRoute from "./components/routing/UserRoute";
+import VentasRoute from "./components/routing/VentasRoute";
+
+//pages
+import Comprar from "./components/pages/Comprar";
+import Ventas from "./components/pages/Ventas";
+import User from "./components/pages/User";
+import Products from "./components/pages/Products";
+import Home from "./components/pages/Home";
+import Private from "./components/pages/Private";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import ForgotPassword from "./components/pages/ForgotPassword";
+import ResetPassword from "./components/pages/ResetPassword";
+const App=()=> {
+  require('dotenv').config()
+  return (
+    <Router>
+
+        <div>
+<Switch>
+
+  <VentasRoute exact path="/comprar" component={Comprar}/>
+  <VentasRoute exact path="/ventas" component={Ventas}/>
+  <UserRoute exact path="/user" component={User}/>
+<PrivateRoute exact path="/private" component={Private} />
+<HomeRoute path="/home" component={Home} />
+<ProductsRoute path="/products" component={Products}/>
+<Route exact path="/login" component={Login}></Route>
+<Route exact path="/register" component={Register}/>
+<Route exact path="/forgotpassword" component={ForgotPassword}/>
+<Route exact path="/passwordreset/:resetToken" component={ResetPassword}/>
+</Switch>
+</div>
+
+      
+    </Router>
+ 
+  );
+}
+
+export default App;
---
+++ src/components/pages/Login.js
@@ -0,0 +1,120 @@
+
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import '../css/main.css'
+import '../css/formatoHome.css'
+import '../plugins/fontawesome-free/css/all.min.css'
+import'../dist/css/adminlte.min.css'
+
+import { Link } from "react-router-dom";
+const BASE_URL = `${process.env.REACT_APP_API_BASE_URL}`;
+
+const PATH_AUTH = `${process.env.REACT_APP_API_AUTH_PATH}`;
+
+const Login = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      history.push("/login");
+    }
+  }, [history]);
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const { data } = await axios.post(
+        `${BASE_URL}${PATH_AUTH}/login`,
+        { email, password },
+        config
+      );
+
+      localStorage.setItem("authToken", data.token);
+
+      history.push("/home");
+    } catch (error) {
+      setError(error.response.data.error);
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  return (
+    <div className="login-screen">
+      <form onSubmit={loginHandler} className="login-screen__form">
+        <h1>AXIS</h1>
+        
+        {error && <span className="error-message">{error}</span>}
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <br/>
+          <div>
+          <input
+            type="email"
+            required
+            id="email"
+           
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            tabIndex={1}
+          />
+          </div>
+          <br/>
+        </div>
+        <div >
+          <label>
+            Password:{" "}
+            
+          </label>
+          <div>
+          <input
+            type="password"
+            required
+            id="password"
+            autoComplete="true"
+           
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            tabIndex={2}
+          />
+          </div>
+         
+          <div>
+          <Link to="/forgotpassword" >
+              Forgot Password?
+            </Link>
+            </div>
+        </div>
+        <br/>
+        <div>
+        <button id="login" type="submit" className="btn btn-primary">
+          Login
+        </button>
+        </div>
+        <br/>
+        <div>
+        <button id="google" type="submit" className="btn btn-primary">
+          Ingresar con Gmail
+        </button>
+        </div>
+
+        <span className="login-screen__subtext">
+          Don't have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
